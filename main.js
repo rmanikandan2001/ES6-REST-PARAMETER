ebe1028ae21ea89452e5bf6995bff4d24b4cf628
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 00:27:39 -0700

    add

---
+++ main.js
@@ -0,0 +1,22 @@
+//REST PARAMETER
+
+
+function sum(result,...numbers){
+	
+	let total=0;
+	
+	for(let number of numbers){
+		
+		total+=number;
+		
+	}
+	sum("total", 1,2,3);
+
+console.log(total);
+}
+
+
+
+
+
+	
